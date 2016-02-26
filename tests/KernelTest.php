1e48773b3d5a1bd6d13aadc3ced9ac732e919f68
Emad Omar <emad2030@gmail.com>
2016-02-26 17:32:30 +0300

    tests: Test kernel with various update inputs

--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -1,5 +1,7 @@
 <?php
+
 use Telebot\Kernel;
+use Telebot\Exceptions\InvalidUpdateInputException;
 
 /**
  * Created by PhpStorm.
@@ -9,12 +11,37 @@ use Telebot\Kernel;
  */
 class KernelTest extends TestCase {
 
-    public function testKernel() {
+    protected $kernel;
+
+    protected function setUp() {
         $botToken = $this->getVar('botToken');
 
-        $kernel = new Kernel($botToken);
+        $this->kernel = new Kernel($botToken);
+        $this->kernel->setErrorHandler(false);
+    }
 
-        $this->assertInstanceOf(Kernel::class, $kernel);
+    public function testEmptyUpdateInput() {
+        // When calling `Kernel#processUpdate()` with a null update (or with no arguments)
+        // It'll attempt to get the update data from `php://input`.
+        // Unfortunately, this cannot be mocked up, because `php://input` is read only.
+        // Therefore we can only assert it's failure.
+        $this->setExpectedException(InvalidUpdateInputException::class);
+
+        $this->kernel->processUpdate();
+    }
+
+    public function testInvalidJsonData() {
+        // Will be thrown due to the invalid json data string that is passed to `processUpdate()`
+        $this->setExpectedException(InvalidUpdateInputException::class);
+
+        $this->kernel->processUpdate(">_<");
+    }
+
+    public function testInvalidUpdateObject() {
+        // Will be thrown due to the invalid update object that is passed to `processUpdate()`
+        $this->setExpectedException(InvalidUpdateInputException::class);
+
+        $this->kernel->processUpdate(new \StdClass);
     }
 
 }
